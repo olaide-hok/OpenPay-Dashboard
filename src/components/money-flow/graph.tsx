81835bec9143ee0d924f35a4738244a3159208a9
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-03-13 15:46:53 +0100

    Create Graph component to render charts

---
+++ src/components/money-flow/graph.tsx
@@ -0,0 +1,17 @@
+import {Flex, Text, VStack} from '@chakra-ui/react'
+
+type Props = {}
+
+const Graph = (props: Props) => {
+  return (
+    <VStack>
+      <Flex>
+        <Text>Money Flow</Text>
+        {/* Icon */}
+      </Flex>
+      {/* Graph */}
+    </VStack>
+  )
+}
+
+export default Graph
