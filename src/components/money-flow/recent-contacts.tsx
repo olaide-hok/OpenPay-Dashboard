05a06396ff78bcc58ec3402659bfbba28d6854fe
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-03-13 15:47:52 +0100

    Create RecentContacts component
    This renders recent contacts.

---
+++ src/components/money-flow/recent-contacts.tsx
@@ -0,0 +1,19 @@
+import {Flex, Text, VStack} from '@chakra-ui/react'
+
+type Props = {}
+
+const RecentContacts = (props: Props) => {
+  return (
+    <VStack>
+      <Flex>
+        <Text>Recent Contacts</Text>
+        {/* Pen and Search Icon */}
+      </Flex>
+      <Text>18 recipients</Text>
+      {/* Contact Avatar Slider */}
+      {/* Group Party  */}
+    </VStack>
+  )
+}
+
+export default RecentContacts
