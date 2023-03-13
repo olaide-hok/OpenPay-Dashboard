b56c7542128337f5d019b08a2e050c76be18ac49
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-03-13 16:04:17 +0100

    Added other layouts of the design

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import {HStack} from '@chakra-ui/react'
-import React from 'react'
+import {HStack, VStack} from '@chakra-ui/react'
+import MoneyFlow from './components/money-flow'
+import MyCard from './components/mycard'
 import {Sidebar} from './components/sidebar'
+import TopNav from './components/top-nav'
 
 type Props = {}
 
@@ -8,6 +10,17 @@ const App = (props: Props) => {
   return (
     <HStack width="full" flex={1} overflow="hidden">
       <Sidebar />
+      <VStack>
+        {/* Search Icon, Bell icon, Avatar */}
+        <TopNav />
+        {/* MyCard and Money Flow Side-by-Side */}
+        <HStack>
+          {/* My Card */}
+          <MyCard />
+          {/* Money Flow */}
+          <MoneyFlow />
+        </HStack>
+      </VStack>
     </HStack>
   )
 }
