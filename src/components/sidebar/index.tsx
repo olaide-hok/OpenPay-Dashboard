14a345ec4a8a544268b9453bce60847bf07931ff
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-03-13 21:04:52 +0100

    Complete SIdebar Component

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,31 +1,48 @@
 import {List, ListItem, VStack} from '@chakra-ui/react'
 import {Logo} from '../logo'
-import {navItems} from './nav-data'
+import {navItems1, navItems2} from './nav-data'
 import NavItem from './nav-item'
 
 export const Sidebar = () => {
   return (
     <VStack
       alignItems="flex-start"
+      justifyContent="space-between"
       width="full"
       height="full"
       maxW={{base: 56, '2xl': 72}}
-      //   borderRightColor="gray.dark"
-      //   borderRightWidth={2}
-      //   flexShrink={0}
-    >
-      {/* Logo */}
-      <Logo />
-      <List width="full" overflowY="auto">
-        {navItems.map((item, index) => {
-          return (
-            <ListItem key={item.label}>
-              {/* NavItem */}
-              <NavItem />
-            </ListItem>
-          )
-        })}
-      </List>
+      borderRightColor="#E8ECF5"
+      borderRightWidth={2}
+      flexShrink={0}
+      pl="5"
+      pt="6"
+      pb="7">
+      <VStack>
+        {/* Logo */}
+        <Logo />
+        <List width="full" spacing="15px">
+          {navItems1.map((item) => {
+            return (
+              <ListItem key={item.label}>
+                {/* NavItem */}
+                <NavItem label={item.label} icon={item.icon} />
+              </ListItem>
+            )
+          })}
+        </List>
+      </VStack>
+      <VStack>
+        <List width="full" spacing="15px">
+          {navItems2.map((item) => {
+            return (
+              <ListItem key={item.label}>
+                {/* NavItem */}
+                <NavItem label={item.label} icon={item.icon} />
+              </ListItem>
+            )
+          })}
+        </List>
+      </VStack>
     </VStack>
   )
 }
