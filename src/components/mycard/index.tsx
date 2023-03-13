3e829fd28d8080269cbbfdbddda6a20f7a6ffad8
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-03-13 15:57:35 +0100

    Create MyCard component
    Renders the Debit card

---
+++ src/components/mycard/index.tsx
@@ -0,0 +1,19 @@
+import {Heading, Text, VStack} from '@chakra-ui/react'
+
+type Props = {}
+
+const MyCard = (props: Props) => {
+  return (
+    <VStack>
+      <Heading>My Card</Heading>
+      {/* Debit Card Component */}
+      <Text>Send Money</Text>
+
+      {/* Send Money Button */}
+
+      {/* Quick Transfer Component */}
+    </VStack>
+  )
+}
+
+export default MyCard
