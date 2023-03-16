130633cc125acd191d8f7566e12d627ee66fe322
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-03-16 23:19:34 +0100

    Create component for Transaction Item

--- src/components/money-flow/recent-trans.tsx
+++ src/components/money-flow/recent-trans.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import {Flex, HStack, Image, Link, Text, VStack} from '@chakra-ui/react'
+import Btn from '../Button'
+import {recentTransData} from './recent-trans-data'
+
+interface TransItemProps {
+  img: string
+  alt: string
+  desc: string
+  label: string
+  date: string
+  amt: string
+}
+
+const TransItem = ({img, alt, label, date, amt, desc}: TransItemProps) => {
+  return (
+    <Flex justifyContent="space-between">
+      <HStack spacing="12px">
+        <Image src={img} alt={alt} />
+        <Text fontSize="10px">{desc}</Text>
+      </HStack>
+      <Text fontSize="10px">{date}</Text>
+      <Text fontSize="10px">{amt}</Text>
+      <Btn variant="outline" label={label} />
+    </Flex>
+  )
+}
 
 type Props = {}
 
 const RecentTrans = (props: Props) => {
-  return <div>RecentTrans</div>
+  return (
+    <VStack alignItems="flex-start">
+      <HStack justifyContent="space-between" w="full">
+        <Text fontSize="15px">Recent Transactions</Text>
+        <Text fontSize="10px" as={Link}>
+          View all
+        </Text>
+      </HStack>
+      <VStack>
+        {recentTransData.map((recentTrans) => {
+          const {img, alt, btnType, date, amt, desc} = recentTrans
+          return (
+            <TransItem
+              img={img}
+              alt={alt}
+              label={btnType}
+              date={date}
+              amt={amt}
+              desc={desc}
+              key={alt}
+            />
+          )
+        })}
+      </VStack>
+    </VStack>
+  )
 }
 
 export default RecentTrans
