654372edc62bfbd0c80fe88e542b2cdfee9f433e
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-03-17 21:19:16 +0100

    Create actionData for MyCard section

---
+++ src/components/mycard/action-tile-data.ts
@@ -0,0 +1,26 @@
+export const actionData = [
+  {
+    src: '/images/send-icon.png',
+    alt: 'send',
+    label: 'Send',
+    variant: 'vertical',
+  },
+  {
+    src: '/images/receive-icon.png',
+    alt: 'receive',
+    label: 'Receive',
+    variant: 'vertical',
+  },
+  {
+    src: '/images/invoicing-logo.png',
+    alt: 'invoicing',
+    label: 'Invoicing',
+    variant: 'vertical',
+  },
+  {
+    src: '/images/more-logo.png',
+    alt: 'more',
+    label: 'More',
+    variant: 'vertical',
+  },
+]
