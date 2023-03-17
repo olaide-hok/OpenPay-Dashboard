bf81a08a100b6249aeb4dc4d44d5ffaad1aaf6fb
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-03-17 21:20:23 +0100

    Create file to hold data for Statistics component

---
+++ src/components/money-flow/stats-action-tile-data.ts
@@ -0,0 +1,30 @@
+export const statsActionData = [
+  {
+    src: '/images/customers-logo.png',
+    alt: 'customers',
+    label: 'customer',
+    variant: 'horizontal',
+    amt: '220k',
+  },
+  {
+    src: '/images/remittance-logo.png',
+    alt: 'Remittance',
+    label: 'Remittance',
+    variant: 'horizontal',
+    amt: '230k',
+  },
+  {
+    src: '/images/donation-logo.png',
+    alt: 'Donation',
+    label: 'Donation',
+    variant: 'horizontal',
+    amt: '270k',
+  },
+  {
+    src: '/images/watchtime-logo.png',
+    alt: 'Wartchtime',
+    label: 'Wartchtime',
+    variant: 'horizontal',
+    amt: '130k',
+  },
+]
