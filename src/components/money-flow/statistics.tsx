356c0d3bff04dea38ede4513367e867e48d02aa0
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-03-17 21:15:13 +0100

    Complete Statistics component

--- src/components/money-flow/statistics.tsx
+++ src/components/money-flow/statistics.tsx
@@ -1,9 +1,37 @@
+import {Box, Heading, HStack, VStack} from '@chakra-ui/react'
 import React from 'react'
+import ActionTile from '../action-tile'
+import {statsActionData} from './stats-action-tile-data'
 
-type Props = {}
-
-const Statistics = (props: Props) => {
-  return <div>Statistics</div>
+const Statistics = () => {
+  return (
+    <Box
+      boxShadow="2xl"
+      bg="white"
+      borderRadius="10px"
+      py="42px"
+      pl="29"
+      pr="34px"
+      w="full">
+      <VStack alignItems="flex-start" spacing="50px">
+        <Heading fontSize="16px">Statistics</Heading>
+        <HStack justifyContent="space-between" w="full">
+          {statsActionData.map((stat) => {
+            const {src, alt, variant, label, amt} = stat
+            return (
+              <ActionTile
+                src={src}
+                alt={alt}
+                amt={amt}
+                label={label}
+                variant={variant}
+              />
+            )
+          })}
+        </HStack>
+      </VStack>
+    </Box>
+  )
 }
 
 export default Statistics
