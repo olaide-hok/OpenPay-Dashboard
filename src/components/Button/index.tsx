9a3900e3d86eb462f08902ba630ec4fd4d498d63
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-03-17 21:21:01 +0100

    Update Btn Component style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,43 @@
-import {Button} from '@chakra-ui/react'
+import {Button, color} from '@chakra-ui/react'
 
 interface ButtonProps {
-  label: string
+  label: 'Success' | 'Pending' | 'Send Money' | string
   variant: string
 }
 
 const Btn = ({label, variant}: ButtonProps) => {
-  const paddingY = variant === 'solid' ? '15px' : '4.5px'
-  const paddingX = variant === 'solid' ? '70px' : '12px'
+  const paddingY = variant === 'solid' ? '15px' : '4px'
+  const paddingX = variant === 'solid' ? '70px' : '10rdpx'
+  const fontSize = variant === 'solid' ? '16px' : '10px'
+  const fontColor =
+    label === 'Send Money'
+      ? 'white'
+      : label === 'Success'
+      ? '#61BB84'
+      : label === 'Pending'
+      ? '#FFAA90'
+      : 'black'
+
+  const brdRadius = variant === 'solid' ? '16px' : '4px'
+  const bgColor =
+    label === 'Success'
+      ? '#EBFFF3'
+      : label === 'Pending'
+      ? '#FFF1ED'
+      : '#0177FB'
+
+  const width = variant === 'outline' ? '57px' : 'null'
 
   return (
-    <Button variant={variant} py={paddingY} px={paddingX} fontSize="10px">
+    <Button
+      borderRadius={brdRadius}
+      variant={variant}
+      bgColor={bgColor}
+      py={paddingY}
+      px={paddingX}
+      color={fontColor}
+      fontSize={fontSize}
+      w={width}>
       {label}
     </Button>
   )
