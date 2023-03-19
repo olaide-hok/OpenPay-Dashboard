dfa1279090ef2181415cb51bc8015b09daf6d437
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-03-19 22:30:31 +0100

    Create data item to map in recent contacts section

---
+++ src/components/money-flow/recent-contacts-data.ts
@@ -0,0 +1,52 @@
+export const recentContacts = [
+  {
+    name: 'Anabel',
+    src: '/images/contacts/rc-1.png',
+    size: 'md',
+  },
+  {
+    name: 'Dean Smith',
+    src: '/images/contacts/rc-2.png',
+    size: 'md',
+  },
+  {
+    name: 'Mark',
+    src: '/images/contacts/rc-3.png',
+    size: 'md',
+  },
+  {
+    name: 'Sean',
+    src: '/images/contacts/rc-4.png',
+    size: 'md',
+  },
+  {
+    name: 'Mount',
+    src: '/images/contacts/rc-5.png',
+    size: 'md',
+  },
+  {
+    name: 'Anabel',
+    src: '/images/contacts/rc-6.png',
+    size: 'md',
+  },
+  {
+    name: 'Dean Smith',
+    src: '/images/contacts/rc-7.png',
+    size: 'md',
+  },
+  {
+    name: 'Mark',
+    src: '/images/contacts/rc-8.png',
+    size: 'md',
+  },
+  {
+    name: 'Sean',
+    src: '/images/contacts/rc-9.png',
+    size: 'md',
+  },
+  {
+    name: 'Mount',
+    src: '/images/contacts/rc-10.png',
+    size: 'md',
+  },
+]
