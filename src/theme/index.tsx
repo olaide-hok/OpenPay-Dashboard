f72a88fb12dc65d36ab1cbdb20efaf82de127b91
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-03-19 03:08:34 +0100

    Crear theme file

---
+++ src/theme/index.tsx
@@ -0,0 +1,11 @@
+import {extendTheme, theme as base} from '@chakra-ui/react'
+
+const theme = extendTheme({
+  colors: {},
+  fonts: {
+    // heading: `Sans-Serif, ${base.fonts?.heading}`,
+    // body: `Mono, ${base.fonts?.body}`,
+  },
+})
+
+export default theme
