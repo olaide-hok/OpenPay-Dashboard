088353c229f77e9ea42ea86b26c33f859c13e8d9
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-03-19 04:04:51 +0100

    Complete TopNav component

--- src/components/top-nav/index.tsx
+++ src/components/top-nav/index.tsx
@@ -1,17 +1,24 @@
-import {Flex} from '@chakra-ui/react'
+import {SearchIcon} from '@chakra-ui/icons'
+import {Box, HStack, Image} from '@chakra-ui/react'
+import AvatarComponent from '../avatar'
 
-type Props = {}
-
-const TopNav = (props: Props) => {
+const TopNav = () => {
   return (
-    <Flex alignItems="flex-end">
-      {/* Search Icon */}
-      Search
-      {/* Bell and badge Icon */}
-      Bell-Badge
-      {/* Avatar */}
-      Avatar
-    </Flex>
+    <Box
+      display="flex"
+      justifyContent="flex-end"
+      alignItems="center"
+      w="full"
+      pt="20px">
+      <HStack spacing="20px">
+        {/* Search Icon */}
+        <SearchIcon />
+        {/* Bell and badge Icon */}
+        <Image src="/images/bell-badge.png" alt="bell-badge-icon" />
+        {/* Avatar */}
+        <AvatarComponent src="/images/derick.png" name="derick" />
+      </HStack>
+    </Box>
   )
 }
 
