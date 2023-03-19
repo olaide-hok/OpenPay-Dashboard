0dc0d323a156c6fcb4cd979fa8c21a432ffe8d3a
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-03-19 22:28:39 +0100

    Add size prop to AvatarComponent

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -3,10 +3,11 @@ import {Avatar} from '@chakra-ui/react'
 interface AvatarProps {
   name: string
   src: string
+  size: string
 }
 
-const AvatarComponent = ({name, src}: AvatarProps) => {
-  return <Avatar name={name} src={src} size="xs" />
+const AvatarComponent = ({name, src, size}: AvatarProps) => {
+  return <Avatar name={name} src={src} size={size} />
 }
 
 export default AvatarComponent
