9214720eeed323f57be21bfab1733d7e499f0eca
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-03-21 15:32:08 +0100

    Refactor TopNav component.
    Pass prop to handle sidebar toggling on small screen.

--- src/components/top-nav/index.tsx
+++ src/components/top-nav/index.tsx
@@ -1,22 +1,46 @@
 import {SearchIcon} from '@chakra-ui/icons'
-import {Box, HStack, Image} from '@chakra-ui/react'
+import {
+  Box,
+  HStack,
+  IconButton,
+  Image,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from '@chakra-ui/react'
 import AvatarComponent from '../avatar'
+import {GrMenu} from 'react-icons/gr'
 
-const TopNav = () => {
+interface Prop {
+  onSidebarOpen: () => void
+}
+
+const TopNav = ({onSidebarOpen}: Prop) => {
   return (
-    <Box
-      display="flex"
-      justifyContent="flex-end"
-      alignItems="center"
-      w="full"
-      pt="20px">
-      <HStack spacing="20px">
-        {/* Search Icon */}
-        <SearchIcon color="#C1C1C1" />
-        {/* Bell and badge Icon */}
-        <Image src="/images/bell-badge.png" alt="bell-badge-icon" />
-        {/* Avatar */}
-        <AvatarComponent src="/images/derick.png" name="derick" size="xs" />
+    <Box as="nav" display="flex" w="full" pt="20px">
+      <HStack
+        spacing="20px"
+        w="full"
+        justifyContent={{base: 'space-between', lg: 'flex-end'}}
+        alignItems="center">
+        <IconButton
+          onClick={onSidebarOpen}
+          display={{base: 'inherit', lg: 'none'}}
+          icon={<GrMenu />}
+          aria-label="Toggle Sidebar"
+        />
+        <HStack>
+          {/* Search Icon */}
+          <InputGroup>
+            <Input border="none" width="auto" size="md" />
+            <InputRightElement children={<SearchIcon color="#C1C1C1" />} />
+          </InputGroup>
+
+          {/* Bell and badge Icon */}
+          <Image src="/images/bell-badge.png" alt="bell-badge-icon" />
+          {/* Avatar */}
+          <AvatarComponent src="/images/derick.png" name="derick" size="xs" />
+        </HStack>
       </HStack>
     </Box>
   )
