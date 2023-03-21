449304cbcdf5a6ffcc1edf7ba3c22b92b920c34f
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-03-21 15:35:45 +0100

    Restructure App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import {Box, Container, HStack, VStack} from '@chakra-ui/react'
+import {Container, Flex, HStack, useDisclosure, VStack} from '@chakra-ui/react'
 import MoneyFlow from './components/money-flow'
 import MyCard from './components/mycard'
-import {Sidebar} from './components/sidebar'
+import Sidebar from './components/sidebar'
+import SideBarDrawer from './components/sidebar/sidebar-drawer'
 import TopNav from './components/top-nav'
 
 const App = () => {
+  const {
+    isOpen: isSidebarOpen,
+    onOpen: onSidebarOpen,
+    onClose: onSidebarClose,
+  } = useDisclosure()
   return (
     <Container maxW="container.xl">
       <HStack h="100vh" flex={1}>
-        <Sidebar />
+        <Flex
+          as="aside"
+          h="full"
+          w="full"
+          display={{base: 'none', lg: 'flex'}}
+          maxW={{base: '11rem', xl: 'sm'}}>
+          <Sidebar />
+        </Flex>
         <VStack h="full" w="full" alignItems="flex-start" spacing={0}>
           {/* Search Icon, Bell icon, Avatar */}
-          <TopNav />
+          <TopNav onSidebarOpen={onSidebarOpen} />
           {/* MyCard and Money Flow Side-by-Side */}
-          <Box display="flex" alignItems="flex-start" w="full">
+          <Flex alignItems="flex-start" justifyContent="space-between" w="full">
             {/* My Card */}
             <MyCard />
             {/* Money Flow */}
             <MoneyFlow />
-          </Box>
+          </Flex>
         </VStack>
       </HStack>
+      <SideBarDrawer isOpen={isSidebarOpen} onClose={onSidebarClose} />
     </Container>
   )
 }
